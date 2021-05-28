4a368368057884463c095c15aabc93efc68db16e
Richard Armstrong <richarms@ska.ac.za>
2021-05-28 14:19:52 +0200

    remove tigger-lsm-style skymodel code, add code that conforms to katsdpimager skymodel class & API structure

--- src/katsdpmodels/local_sky_model.py
+++ src/katsdpmodels/local_sky_model.py
@@ -14,65 +14,23 @@
 # limitations under the License.
 ################################################################################
 
-"""Local Sky Model"""
+"""Local Sky Models"""
 
-from typing import ClassVar, Any
+from typing import ClassVar, Any, Union
 from typing_extensions import Literal
 
 try:
     from numpy.typing import ArrayLike
 except ImportError:
     ArrayLike = Any  # type: ignore
+
 import astropy.units as u
-from astropy.coordinates import SkyCoord
 import h5py
+import telstate
 
 from . import models
 
-
-class AltAzFrame:
-    """Coordinate system aligned with the antenna.
-
-    The l coordinate is horizontal and increases with increasing azimuth (north
-    to east), while the m coordinate is vertical and increases with increasing
-    altitude. Both are defined by an orthographic (SIN) projection, with the
-    nominal pointing centre at zero.
-    """
-    pass
-
-
-class RADecFrame:
-    """Coordinate system aligned with a celestial sphere.
-
-    The l coordinate is aligned with right ascension and the m coordinate with
-    declination, and increase in the corresponding directions. Both are defined by
-    an orthographic (SIN) projection, with the nominal pointing centre at zero.
-
-    Any RA/Dec system can be used (e.g. ICRS/GCRS/CIRS) as long as the
-    parallactic angle is computed for the same system.
-
-    Parameters
-    ----------
-    parallactic_angle
-        Rotation angle to align the celestial sphere with the antenna. See
-        :meth:`katpoint.Target.parallactic_angle` for a definition.
-    """
-
-    def __init__(self, parallactic_angle: u.Quantity) -> None:
-        self.parallactic_angle = parallactic_angle.to(u.rad)  # Just to verify unit type
-
-    @staticmethod
-    def from_sky_coord(self, target: SkyCoord) -> 'RADecFrame':
-        """Generate a frame from a target (assuming an AltAz mount).
-
-        The `target` must have ``obstime`` and ``location`` properties, and
-        must be scalar. It will be converted to ICRS if necessary.
-        """
-        # TODO: implement
-        raise NotImplementedError
-
-
-class LocalSkyModel(models.Model):
+class LocalSkyModel(models.SimpleHDF5Model):
     """ Base class for sky models """
     model_type: ClassVar[Literal['lsm']] = 'lsm'
     # Methods are not marked @abstractmethod as it causes issues with mypy:
@@ -84,3 +42,80 @@ class LocalSkyModel(models.Model):
 
     def to_hdf5(self, hdf5: h5py.File) -> None:
         raise NotImplementedError()      # pragma: nocover
+
+def catalogue_from_telstate(telstate: Union[katsdptelstate.TelescopeState, katdal.sensordata.TelstateToStr],
+                            capture_block_id: str,
+                            continuum: Union[str, None],
+                            target: katpoint.Target) -> katpoint.Catalogue:
+    """Extract a katpoint catalogue written by katsdpcontim.
+    Parameters
+    ----------
+    telstate : :class:`katsdptelstate.TelescopeState` or :class:`katdal.sensordata.TelstateToStr`
+        Telescope state
+    capture_block_id : str
+        Capture block ID
+    continuum : str or ``None``
+        Name of the continuum imaging stream (used to form the telstate view).
+        If ``None``, there must be exactly one continuum imaging stream in the
+        data set, which is used.
+    target : :class:`katpoint.Target`
+        Target field
+    Raises
+    ------
+    NoSkyModelError
+        If no sky model could be found for the given parameters
+    Returns
+    -------
+    katpoint.Catalogue
+    """
+    from katdal.sensordata import TelstateToStr
+
+    telstate = TelstateToStr(telstate)
+    try:
+        # Find the continuum image stream
+        if continuum is None:
+            archived_streams = telstate['sdp_archived_streams']
+            for stream_name in archived_streams:
+                view = telstate.view(stream_name, exclusive=True)
+                view = view.view(telstate.join(capture_block_id, stream_name))
+                stream_type = view.get('stream_type', 'unknown')
+                # The correct value is 'sdp.continuum_image', but due to a bug
+                # there are observations in the wild with just 'continuum_image'.
+                if stream_type not in {'sdp.continuum_image', 'continuum_image'}:
+                    continue
+                if continuum is not None:
+                    raise NoSkyModelError(
+                        'Multiple continuum image streams found - need to select one')
+                continuum = stream_name
+            if continuum is None:
+                raise NoSkyModelError('No continuum image streams found')
+
+        view = telstate.view(continuum, exclusive=True)
+        view = view.view(telstate.join(capture_block_id, continuum))
+        target_namespace = view['targets'][target.description]
+        prefix = telstate.join(capture_block_id, continuum, target_namespace, 'target0')
+        data = view.view(prefix)['clean_components']
+        # Should always match, but for safety
+        if katpoint.Target(data['description']) == target:
+            return katpoint.Catalogue(data['components'])
+    except KeyError:
+        logger.debug('KeyError', exc_info=True)
+    raise NoSkyModelError('Sky model for target {} not found'.format(target.name))
+
+class ComponentSkyModel(LocalSkyModel):
+    model_format: ClassVar[Literal['ranges']] = 'skymodel'
+
+    def __init__(self,):
+
+    @classmethod
+    def from_hdf5(cls: Type[_P], hdf5: h5py.File) -> _P:
+        cat = models.get_hdf5_dataset(hdf5, 'catalogue')
+
+        return cls(cat)
+
+    def to_hdf5(self, hdf5: h5py.File) -> None:
+        hdf5.attrs['TODO: attributes'] = self.PROPERTIES
+        # The constructor ensures we're using unscaled units
+        hdf5.create_dataset('cat', data=self.cat, track_times=False)
+
+
